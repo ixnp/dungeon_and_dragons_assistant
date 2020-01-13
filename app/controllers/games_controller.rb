a99dd04aca837117722b67035e9ddbe5e73bf7f4
ixnp <enprocopios@gmail.com>
2020-01-13 13:32:01 -0800

    added flash errors to game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,20 @@
 class GamesController < ApplicationController
-def new
-    @game = Game.new
-end
+    def new
+        @game = Game.new
+    end
+    
+    def create 
+        @game = Game.new
+        @game.save
+        if @game.save
+        flash[:notice] = "Let the advanture begin!"
+        redirect_to games_show(@game)
+        else 
+            render 'new'
+        end
+    end
+    private
+    def game_params
+        params.require(:game).permit(:title,:lore,:rules)
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,16 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    def create
+        @user = User.new(article_params)
+        @user.save
+        redirect_to user_path(@user)
+    end 
+  
+    private
+  
+    def user_params
+      params.require(:user).permit(:name)
+    end 
 end
