a6018ef73a243256e890a151b41b05a894c76f0d
ixnp <enprocopios@gmail.com>
2020-01-15 14:09:08 -0800

    added session on user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     
     if @user.save
+    session[:user_id] = @user.id
     flash[:success] = "Welcome To DnD Helper.inc"
     
     redirect_to user_path(@user)
